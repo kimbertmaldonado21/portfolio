d710d637b87c949db0fd2f3f113f50d01853516e
Kimbert Maldonado <kimbert.m.maldonado@gmail.com>
2022-08-31 01:49:04 +0800

    Start up Navigation design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from "react";
+
 
 function App() {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(()=>{
+    window.onscroll = () => { 
+      if(window.pageYOffset > 0){
+        setIsScrolled(true)
+        console.log(isScrolled)
+      }else{
+        setIsScrolled(false)
+        console.log(isScrolled)
+      } 
+    }
+    return () => (window.onscroll = null);
+  })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="App" >
+      <div id="particles-js" className={isScrolled? "Navigation active" : "Navigation"} >
+
+        <div className="Header"><h2 data-text="I Am Kimbert">I Am Kimbert </h2></div>
+        {/* <div className="Header"><p>This website is dedicated to myself as i built it with HTML CSS Javascript</p></div> */}
+        <a href="#home">Home</a>
+        <a href="#About">About</a>
+        <a href="#Resume">Resume</a>
+        <a href="#Contact">Contact</a>
+
+        <a href="#" className="icon">
+          <i class="fa fa-bars"></i>
         </a>
-      </header>
+
+      </div>
+
+      <div className="Content">
+
+      </div>
     </div>
   );
 }
